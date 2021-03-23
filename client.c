a2af26e6401b68d9b11df0d9946e7f03ae90da2a
ALLAL abderrahmane <abderrahmane.allal@etu.u-paris.fr>
2021-03-23 21:09:56 +0100

    Add some unecessary files for now

---
+++ client.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main() {
+
+    
+    return 0;
+}
