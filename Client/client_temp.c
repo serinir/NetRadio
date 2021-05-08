4946f3441444a6e5d4cce0a352025c0005b0ef8c
ALLAL abderrahmane <abderrahmane.allal@etu.u-paris.fr>
2021-05-08 02:50:14 +0200

    Update client_temp.c

--- Client/client_temp.c
+++ Client/client_temp.c
@@ -12,10 +12,17 @@
 #include <fcntl.h>
 
 #define LEN_OLDM 159
+#define LEN_ITEM 55
 
-
+void * chat_gestionnaire(void * arg);
 void * lecture_term(void * arg);
 void * ecriture_term(void * arg);
+
+char ip1[17];
+char port1[7];
+
+char ip2[17];
+char port2[7];
 
 char id[12];
 
@@ -62,24 +69,59 @@ int main() {
 
 void * lecture_term(void * arg) {
 
-    // char cmd[3];
-
-    char mess[150];
-    char nb_mess[5];
-    char cmdToSend[170];
+    char * mess = malloc(sizeof(char) * 150);
+    char * nb_mess = malloc(sizeof(char) * 5);
+    char * cmdToSend = malloc(sizeof(char) * 170);
 
     char rep[5];
 
-    struct sockaddr_in adress_sock;
-    adress_sock.sin_family = AF_INET;
-    adress_sock.sin_port = htons(5051);
-    inet_aton("127.0.0.1", &adress_sock.sin_addr);
+    // CREATION SOCKET GESTIONNAIRE
+    struct sockaddr_in adress_sock_gest;
+    adress_sock_gest.sin_family = AF_INET;
+    adress_sock_gest.sin_port = htons(5055);
+    inet_aton("127.0.0.1", &adress_sock_gest.sin_addr);
 
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    int sock_gest = socket(PF_INET, SOCK_STREAM, 0);
 
-    int r = connect(sock, (struct sockaddr *) &adress_sock, sizeof(struct sockaddr_in));
+    int r_gest = connect(sock_gest, (struct sockaddr *) &adress_sock_gest, sizeof(struct sockaddr_in));
 
-    if(r != -1) {
+    if(r_gest != -1) {
+        char * currentItem = malloc(LEN_ITEM * sizeof(char)+4);
+        char * linb = malloc(15*sizeof(char));
+
+        sprintf(cmdToSend, "LIST\r\n");
+
+        send(sock_gest, cmdToSend, strlen(cmdToSend)*sizeof(char), 0);
+
+        int size_rec_gest = 0;
+
+        size_rec_gest = recv(sock_gest, linb, 12*sizeof(char), 0);
+        printf("%s\n", linb);
+
+        do {
+            size_rec_gest = recv(sock_gest, currentItem, LEN_ITEM*sizeof(char)+15, 0); // Reception des OLD MESSAGES
+            currentItem[size_rec_gest] = '\0';
+            strncpy(ip1, currentItem+14, 15);
+            strncpy(port1, currentItem+30, 4);
+            strncpy(ip2, currentItem+35, 15);
+            strncpy(port2, currentItem+51, 4);
+            printf("%s\n", currentItem);
+        } while(size_rec_gest != 0);
+
+    }
+
+    int sock_diff = socket(PF_INET, SOCK_STREAM, 0);
+
+    // CREATION SOCKET DIFFUSEUR
+    struct sockaddr_in adress_sock_diff;
+    adress_sock_diff.sin_family = AF_INET;
+    adress_sock_diff.sin_port = htons(atoi(port2));
+    inet_aton(ip2, &adress_sock_diff.sin_addr);
+
+
+    int r_diff = connect(sock_diff, (struct sockaddr *) &adress_sock_diff, sizeof(struct sockaddr_in));
+
+    if(r_diff != -1) {
 
         int r=0;
 
@@ -94,6 +136,7 @@ void * lecture_term(void * arg) {
             if(r == 'M') { // Case M
 
                 while(1) {
+                    
                     r = read(0, mess, 150); // Retourne taille de l'input + 1
                     if(r<=141) break;
                 }
@@ -111,15 +154,14 @@ void * lecture_term(void * arg) {
     
                 sprintf(cmdToSend, "MESS %s %s\r\n", id, mess);
                 
-                send(sock, cmdToSend, strlen(cmdToSend)*sizeof(char), 0); // Envoie de MESS id message
-                int size_rec = recv(sock, rep, 5*sizeof(char), 0); // Reception de ACKM
+                send(sock_diff, cmdToSend, strlen(cmdToSend)*sizeof(char), 0); // Envoie de MESS id message
+                int size_rec = recv(sock_diff, rep, 5*sizeof(char), 0); // Reception de ACKM
                 rep[size_rec] = '\0';
                 printf("%s\n", rep);
 
-            } else { // Case L
-
+            } else { // Case L  
                 while(1) {
-                    r = read(0, nb_mess, 5); // Retourne taille de l'input + 1
+                    r = read(0, nb_mess, 5); // Retourne taille de l'input + a
                     if(r<=4) break;
                 }
 
@@ -146,30 +188,30 @@ void * lecture_term(void * arg) {
 
                 sprintf(cmdToSend, "LAST %s\r\n", nb_mess);
 
-                send(sock, cmdToSend, strlen(cmdToSend)*sizeof(char), 0);
+                send(sock_diff, cmdToSend, strlen(cmdToSend)*sizeof(char), 0);
 
                 int size_rec = 0;
 
                 do {
-                    size_rec = recv(sock, currentOldM, LEN_OLDM*sizeof(char)+15, 0); // Reception des OLD MESSAGES
+                    size_rec = recv(sock_diff, currentOldM, LEN_OLDM*sizeof(char)+15, 0); // Reception des OLD MESSAGES
                     currentOldM[size_rec] = '\0';
                     printf("%s\n", currentOldM);
                 } while(size_rec != 0);
+
                 
             }
-
-        close(sock);
+        }
+        close(sock_diff);
+        close(sock_gest);
     }
-            }
             
-
     return NULL;
-
 }
+
 
 void * ecriture_term(void * arg) {
 
-    int fd = open("/dev/pts/2", O_RDWR);
+    int fd = open("/dev/pts/4", O_RDWR);
 
     if(fd==-1) {
         perror("open");
@@ -183,14 +225,14 @@ void * ecriture_term(void * arg) {
 
     struct sockaddr_in address_sock;
     address_sock.sin_family=AF_INET;
-    address_sock.sin_port=htons(5000);
+    address_sock.sin_port=htons(5000); // port de multidiffusion
     address_sock.sin_addr.s_addr=htonl(INADDR_ANY);
 
     r=bind(sock, (struct sockaddr *)&address_sock, 
         sizeof(struct sockaddr_in));
 
     struct ip_mreq mreq;
-    mreq.imr_multiaddr.s_addr=inet_addr("225.10.20.30");
+    mreq.imr_multiaddr.s_addr=inet_addr("225.10.20.30"); // Adresse de multidiffusion
     mreq.imr_interface.s_addr=htonl(INADDR_ANY);
 
     r=setsockopt(sock, IPPROTO_IP, 
